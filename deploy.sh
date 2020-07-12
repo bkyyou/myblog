4af3e37b9881f59d08b9cb9e7080bd53ef8ec505
bikeyou <laobi08@126.com>
2020-07-12 22:45:58 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,21 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:bkyyou/myblog.git master:gh-pages
-# 链接远程仓库
-git remote add origin https://github.com/bkyyou/myblog.git
-# 拉取远程仓库的文件
-git pull --rebase origin master  
-# 同步更新代码
+
+# # 链接远程仓库
+# git remote add origin https://github.com/bkyyou/myblog.git
+# # 拉取远程仓库的文件
+# git pull --rebase origin master  
+# # 同步更新代码
+# git push -u origin master
+# cd -
+
+cd ../../../
+
+git add .
+
+git commit -m 'deploy'
+
+git pull origin master
+
 git push -u origin master
-cd -
